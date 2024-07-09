a2016e8cb9c16b9a3331ef42cf27f5fcf45f1a94
Shchipanov <chips_80@mail.ru>
2024-07-09 16:41:07 +0400

    feat(film.js): делает шаблое генерации фильма из данных констант мока

---
+++ src/mock/constant.js
@@ -0,0 +1,90 @@
+const titles = [
+  "Country On Him",
+  "Raiders With The Carpet",
+  "Guest Who Sold The Darkness",
+  "A Tale Of A Little Bird In The Storm",
+  "Friends On The Room",
+  "Raiders Who Stole Us",
+  "Friends Without Themselves",
+  "Pioneers Without Us",
+  "A Man With Themselves",
+  "Guest With The Darkness",
+  "A Little Pony Who Bought The Darkness",
+  "Family Who Bought The Carpet",
+  "Raiders Who Saw Him",
+  "Guest Within Him",
+  "Pioneers Without The Darkness",
+  "A Tale Of A Little Bird With Him",
+  "A Shark Who Sold The Wall",
+  "Raiders Who The Storm",
+  "Family Who Stole The Darkness",
+  "A Lion Without Us",
+];
+
+const Rating = {
+  MIN: 0,
+  MAX: 10,
+};
+
+const posters = [
+  "images/posters/made-for-each-other.png",
+  "images/posters/popeye-meets-sinbad.png",
+  "images/posters/sagebrush-trail.jpg",
+  "images/posters/santa-claus-conquers-the-martians.jpg",
+  "images/posters/the-dance-of-life.jpg",
+  "images/posters/the-great-flamarion.jpg",
+  "images/posters/the-man-with-the-golden-arm.jpg",
+];
+
+const AgeRating = {
+  MIN: 0,
+  MAX: 18,
+};
+
+const names = ["Alice", "Ivan", "Sergey", "Dakota", "Nevada", "Fedor"];
+
+const surnames = ["Makoveev", "Ivanov", "Romanov", "Lee", "James", "Walker"];
+
+const NAME_COUNT = 2;
+
+const YearsDuration = {
+  MIN: 5,
+  MAX: 10,
+};
+
+const countries = [
+  "USA",
+  "Russia",
+  "Germany",
+  "Finland",
+  "France",
+  "Spain",
+  "Italy",
+  "China",
+  "Japan",
+];
+
+const Runtime = {
+  MIN: 60,
+  MAX: 180,
+};
+
+const genres = [
+  "Animation",
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Family",
+  "Horror",
+  "Thriller",
+];
+
+const GenreCount = {
+  MIN: 1,
+  MAX: 3,
+};
+
+const description =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra.";
+
+export {titles, Rating, posters, AgeRating, names, surnames, NAME_COUNT, YearsDuration, countries, Runtime, genres, GenreCount, description};
